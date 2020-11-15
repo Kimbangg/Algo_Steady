dfb2d15ee16be1367e0675f3bd37b806e6e89cf1
Kimbangg <ehehdgus1@hanyang.ac.kr>
2020-11-15 13:32:53 +0900

    파일 정리

--- backjoon/ArrSum.py
+++
@@ -1,4 +0,0 @@
-N, M = map(int, input().split())
-AB = list(map(int, input().split())) + list(map(int, input().split()))
-print(' '.join(map(str, sorted(AB))))
-
--- backjoon/MakingO.py
+++
@@ -1,9 +0,0 @@
-N = int(input())
-dp = [0] * (N + 1)
-card = [0] + list(map(int, input().split()))
-dp[1] = card[1]
-for i in range(2, N + 1):
-    for j in range(1, i + 1):
-        if dp[i] < dp[i - j] + card[j]:
-            dp[i] = dp[i - j] + card[j]
-print(dp[N])
